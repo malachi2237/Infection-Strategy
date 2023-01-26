516338bd971faf4529e9573a13df3c2b76906ada
malachi2237 <35237626+malachi2237@users.noreply.github.com>
2023-01-26 11:11:12 -0800

    Got rid of unnecessary code in character

--- Source/InfectionStrategy/InfectionStrategyCharacter.cpp
+++ Source/InfectionStrategy/InfectionStrategyCharacter.cpp
@@ -14,21 +14,12 @@
 
 AInfectionStrategyCharacter::AInfectionStrategyCharacter()
 {
-	// Set size for player capsule
-	/*GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);*/
-
 	movementComponent = CreateDefaultSubobject<UStrategyMovementComponent>(TEXT("MovementComponent"));
 
 	// Don't rotate character to camera direction
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
-
-	// Configure character movement
-	//GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
-	//GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
-	//GetCharacterMovement()->bConstrainToPlane = true;
-	//GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
 	// Create a camera boom...
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));

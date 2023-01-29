499875cff5f75ae13e93efee86f735aadd7c06bc
malachi2237 <35237626+malachi2237@users.noreply.github.com>
2023-01-28 17:44:09 -0800

    VehicleWidget brought up to coding standards. All cpp files now brought up to standards.

--- Source/InfectionStrategy/VehicleWidget.h
+++ Source/InfectionStrategy/VehicleWidget.h
@@ -10,49 +10,66 @@
 class AVehicleUnit;
 
 DECLARE_DELEGATE(FUiVehicleActionDelegate);
-/**
- * 
- */
+
+/** Base class for HUD for vehicle unit actions */
 UCLASS()
 class INFECTIONSTRATEGY_API UVehicleWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
 public:
-	void SetSelectedVehicle(AVehicleUnit* unit) { selectedVehicle = unit; }
+	/** Sets the vehicle the widget will interact with. */
+	void SetSelectedVehicle(AVehicleUnit* unit) { SelectedVehicle = unit; }
 
+	/** Returns the amount of tile movement remaining for the selected vehicle. */
 	UFUNCTION(BlueprintCallable)
-
 	int32 GetRemainingMovement();
 
-	FUiVehicleActionDelegate OnMovementSelected;
-	FUiVehicleActionDelegate OnAttackSelected;
-
-	FUiVehicleActionDelegate OnDeselect;
-	FUiVehicleActionDelegate OnAttack;
-	FUiVehicleActionDelegate OnMove;
-
+	/** Deselect the vehicle and restore the widget to a deselected state */
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void Deselect();
 
+	/** Callback for when the selected vehicle completes movement. */
 	UFUNCTION(BlueprintImplementableEvent)
 	void OnMovementCompleted();
 
-protected:
-	AVehicleUnit* selectedVehicle;
+	/** Delegate for when movement action is selected */
+	FUiVehicleActionDelegate OnMovementSelected;
 
+	/** Delegate for when attack action is selected */
+	FUiVehicleActionDelegate OnAttackSelected;
+
+	/** Delegate for when vehicle is deselected via the widget */
+	FUiVehicleActionDelegate OnDeselect;
+
+	/** Delegate for when an attack is confirmed via the widget */
+	FUiVehicleActionDelegate OnAttack;
+
+	/** Delegate for when movement is confirmed via the widget */
+	FUiVehicleActionDelegate OnMove;
+
+protected:
+	/** Currently selected vehicle */
+	UPROPERTY()
+	AVehicleUnit* SelectedVehicle;
+
+	/** Executes OnMovementSelected delegate */
 	UFUNCTION(BlueprintCallable)
 	void SendOnMovementSelected();
 
+	/** Executes OnAttackSelected delegate */
 	UFUNCTION(BlueprintCallable)
 	void SendOnAttackSelected();
 
+	/** Executes OnDeselect delegate */
 	UFUNCTION(BlueprintCallable)
 	void SendOnDeselect();
 
+	/** Executes OnAttack delegate */
 	UFUNCTION(BlueprintCallable)
 	void SendOnAttack();
 
+	/** Executes OnMove delegate */
 	UFUNCTION(BlueprintCallable)
 	void SendOnMove();
 };
